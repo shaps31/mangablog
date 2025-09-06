b1b7e205673a5ef45404598008b731deb7aa03d2
Shabadine <bah.shabadine@outlook.fr>
2025-09-06 13:39:19 +0200

    amelioration(post): renseigner author, slug auto et publishedAt en new/edit

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/post')]
 final class PostController extends AbstractController
@@ -23,13 +24,26 @@ final class PostController extends AbstractController
     }
 
     #[Route('/new', name: 'app_post_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager,SluggerInterface $slugger): Response
     {
         $post = new Post();
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Associer l'auteur connecté
+            $post->setAuthor($this->getUser());
+
+            // Générer le slug si vide
+            if (!$post->getSlug()) {
+                $post->setSlug(strtolower($slugger->slug($post->getTitle())));
+            }
+
+            // Si on publie sans date -> maintenant
+            if ($post->getStatus() === 'published' && null === $post->getPublishedAt()) {
+                $post->setPublishedAt(new \DateTimeImmutable());
+            }
+
             $entityManager->persist($post);
             $entityManager->flush();
 
@@ -51,12 +65,25 @@ final class PostController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_post_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Post $post, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Associer l'auteur connecté
+            $post->setAuthor($this->getUser());
+
+            // Générer le slug si vide
+            if (!$post->getSlug()) {
+                $post->setSlug(strtolower($slugger->slug($post->getTitle())));
+            }
+
+            // Si on publie sans date -> maintenant
+            if ($post->getStatus() === 'published' && null === $post->getPublishedAt()) {
+                $post->setPublishedAt(new \DateTimeImmutable());
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_post_index', [], Response::HTTP_SEE_OTHER);
