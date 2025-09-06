34743c42ec504225fe6d58b22dffdb2d00deeab1
Shabadine <bah.shabadine@outlook.fr>
2025-09-06 16:18:53 +0200

    correction(import): ajouter use App\Repository\CommentRepository dans BlogController

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CommentRepository;
 
 final class BlogController extends AbstractController
 {
